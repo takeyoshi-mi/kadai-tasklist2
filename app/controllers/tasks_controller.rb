398326168e459532e22350da46507d7b5815d691
Takeyoshi <hi.its.take@tmizusawa.com>
2017-05-04 14:16:02 +0000

    update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,25 +1,25 @@
 class TasksController < ApplicationController
-  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  before_action :set_tasks, only: [:show, :edit, :update, :destroy]
 
   def index
-    @messages = Message.all
+    @tasks = Task.all
   end
 
   def show
   end
 
   def new
-    @message = Message.new
+    @tasks = Task.new
   end
 
   def create
-    @message = Message.new(message_params)
+    @tasks = Task.new(tasks_params)
 
-    if @message.save
-      flash[:success] = 'Message が正常に投稿されました'
-      redirect_to @message
+    if @tasks.save
+      flash[:success] = 'tasks が正常に投稿されました'
+      redirect_to @tasks
     else
-      flash.now[:danger] = 'Message が投稿されませんでした'
+      flash.now[:danger] = 'tasks が投稿されませんでした'
       render :new
     end
   end
@@ -28,29 +28,29 @@ class TasksController < ApplicationController
   end
 
   def update
-    if @message.update(message_params)
-      flash[:success] = 'Message は正常に更新されました'
-      redirect_to @message
+    if @tasks.update(tasks_params)
+      flash[:success] = 'tasks は正常に更新されました'
+      redirect_to @tasks
     else
-      flash.now[:danger] = 'Message は更新されませんでした'
+      flash.now[:danger] = 'tasks は更新されませんでした'
       render :edit
     end
   end
 
   def destroy
-    @message.destroy
+    @tasks.destroy
 
-    flash[:success] = 'Message は正常に削除されました'
-    redirect_to messages_url
+    flash[:success] = 'tasks は正常に削除されました'
+    redirect_to tasks_url
   end
 
   private
 
-  def set_message
-    @message = Message.find(params[:id])
+  def set_tasks
+    @tasks = Task.find(params[:id])
   end
 
-  def message_params
-    params.require(:message).permit(:content)
+  def tasks_params
+    params.require(:task).permit(:content)
   end
 end
